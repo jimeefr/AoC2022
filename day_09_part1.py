fbbdb82bf44be434a5aec5af5f5209b26adf21b9
jimee <jimee@votez-cthulhu.net>
2022-12-09 07:35:34 +0100

    cleaning

--- day_09_part1.py
+++ day_09_part1.py
@@ -2,8 +2,6 @@ from common import read_input
 
 lines = read_input(9)
 
-tail_pos,head_pos = (0,0),(0,0)
-
 def move(x,y,d):
     if 'R' in d: x+=1
     if 'L' in d: x-=1
@@ -12,22 +10,20 @@ def move(x,y,d):
     return x,y
 
 def follow(x,y,xx,yy):
-    if abs(x-xx)<2 and abs(y-yy)<2: return xx,yy
+    if abs(x-xx) < 2 and abs(y-yy) < 2: return xx,yy
     if abs(x-xx) == 2:
-        xx += (x-xx)//2
-        if abs(y-yy) == 2: yy += (y-yy)//2
-        elif yy != y: yy += (y-yy)
-    if abs(y-yy) ==2:
-        yy += (y-yy)//2
-        if abs(x-xx) == 2: xx += (x-xx)//2
-        elif xx != x: xx += (x-xx)
+        xx += (x-xx) // 2
+        if yy != y: yy += (y-yy)
+    if abs(y-yy) == 2:
+        yy += (y-yy) // 2
+        if xx != x: xx += (x-xx)
     return xx,yy
 
+tail_pos,head_pos = (0,0),(0,0)
 path = [tail_pos]
 for l in lines:
     d,count = l.split()
-    count = int(count)
-    for _ in range(count):
+    for _ in range(int(count)):
         head_pos = move(*head_pos,d)
         tail_pos = follow(*head_pos,*tail_pos)
         path.append(tail_pos)
--- day_09_part2.py
+++ day_09_part2.py
@@ -2,8 +2,6 @@ from common import read_input
 
 lines = read_input(9)
 
-tail_pos,head_pos = (0,0),(0,0)
-
 def move(x,y,d):
     if 'R' in d: x+=1
     if 'L' in d: x-=1
@@ -12,14 +10,14 @@ def move(x,y,d):
     return x,y
 
 def follow(x,y,xx,yy):
-    if abs(x-xx)<2 and abs(y-yy)<2: return xx,yy
+    if abs(x-xx) < 2 and abs(y-yy) < 2: return xx,yy
     if abs(x-xx) == 2:
-        xx += (x-xx)//2
-        if abs(y-yy) == 2: yy += (y-yy)//2
+        xx += (x-xx) // 2
+        if abs(y-yy) == 2: yy += (y-yy) // 2
         elif yy != y: yy += (y-yy)
-    if abs(y-yy) ==2:
-        yy += (y-yy)//2
-        if abs(x-xx) == 2: xx += (x-xx)//2
+    if abs(y-yy) == 2:
+        yy += (y-yy) // 2
+        if abs(x-xx) == 2: xx += (x-xx) // 2
         elif xx != x: xx += (x-xx)
     return xx,yy
 
@@ -27,9 +25,8 @@ rope = [(0,0)] * 10
 path = [(0,0)]
 for l in lines:
     d,count = l.split()
-    count = int(count)
-    for _ in range(count):
+    for _ in range(int(count)):
         rope[0] = move(*rope[0],d)
-        for i in range(9): rope[i+1] = follow(*rope[i],*rope[i+1])
-        path.append(rope[9])
+        for i in range(len(rope)-1): rope[i+1] = follow(*rope[i],*rope[i+1])
+        path.append(rope[-1])
 print(len(set(path)))
